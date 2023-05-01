572925130796164ab07208d51550b33129715766
VartanAvedoumian <132273687+VartanAvedoumian@users.noreply.github.com>
2023-05-01 09:07:42 -0500

    Update code.R

--- code.R
+++ code.R
@@ -17,11 +17,6 @@ movies %>%
 movies$date_added <- mdy(movies$date_added)
 
 
-#Turn rating into factor object
-
-movies$rating <- factor(movies$rating,
-                        levels = c("TV-Y", "TV-Y7", "TV-G", "TVPG", "TV-14", "TV-MA","G", "PG", "PG-13", "R"))
-
 #Answering 1st question:
 #What time of the year are the most movies/TV shows released?
 
@@ -31,3 +26,16 @@ ggplot(movies)+
   labs(x = "Year")+
   ggtitle("Amount of Films released each year")
 
+#Turn rating into factor object
+movies$tv_rating <- factor(movies$rating,
+                           levels = c("TV-Y","TV-Y7", "TV-G", "TV-PG", "TV-14", "TV-MA"))
+
+movies$movie_rating <- factor(movies$rating,
+                              levels = c("G", "PG", "PG-13","R"))
+
+#Creating pie charts to show distribution of ratings
+tv_chart <- table(movies$tv_rating)
+pie(tv_chart)
+
+movie_chart <- table(movies$movie_rating)
+pie(movie_chart)
